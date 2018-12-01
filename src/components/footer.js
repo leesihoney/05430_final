ddd8354233b9f2cf873fbb870756c56e34991742
Cole Gleason <cole@colegleason.com>
2018-12-01 11:02:58 -0500

    remove circular footer dependancy

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 class Footer extends Component {
     render() {
         return (
-            <Footer className = 'footer'>
                 <div className = 'footer-groups'>
                     <div className = 'footer-icons'>
                         <img alt="leesihoney" src = {require('../images/facebook-logo-button.svg')}/>
@@ -14,7 +13,6 @@ class Footer extends Component {
                         all rights reserved with &copy; Andrew Siheon Lee 
                     </div>
                 </div>
-            </Footer>
         )
     }
 }
