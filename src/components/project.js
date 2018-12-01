4ffd6879e0ed4aa7332b617fd07611cc5b116090
Cole Gleason <cole@colegleason.com>
2018-12-01 11:03:25 -0500

    add render method and props to project page

--- src/components/project.js
+++ src/components/project.js
@@ -1,27 +1,39 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
 
-const Project = props => (
+class Project extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+        }
+        this.props = props
+    }
+ render() {
+ return (
     <div className = "project-component">
-        <img alt="leesihoney" src = {props.image} className = "project-pic"/>
+        <img alt="leesihoney" src = {this.props.image} className = "project-pic"/>
 
         <div className = 'project-right'>
             <div className = 'project-inc-position'>
-                {props.company} | {props.position}
+                {this.props.company} | {this.props.position}
             </div>
 
             <div className = 'project-title'>
-                {props.title}
+                {this.props.title}
             </div>
 
             <div className = 'project-description'>
-                {props.exp}
+                {this.props.exp}
             </div>
 
-            <Link to={props.url} className = 'project-button'> View Details </Link>
+            <Link to={this.props.url} className = 'project-button'> View Details </Link>
 
         </div>
     </div>
         )
+ }
+}
 
 export default Project;
